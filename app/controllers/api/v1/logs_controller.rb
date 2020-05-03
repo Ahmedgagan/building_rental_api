35590eb25758550d3deed288d4616c18dd1949db
Ahmed <ahmedgagan3@gmail.com>
2020-05-03 21:59:06 +0530

    logs done

--- app/controllers/api/v1/logs_controller.rb
+++ app/controllers/api/v1/logs_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class LogsController < ApplicationController
       def index
-        logs = Log.joins("INNER JOIN users b on log.admin_user_id=b.user_id").select("*").order('created_at DESC')
+        logs = Log.joins("INNER JOIN users b on logs.admin_user_id=b.id").select("*").order('created_at DESC')
         render json: {status: '1', msg: 'All logs displayed', data:logs}, status: :ok
       end
     end
