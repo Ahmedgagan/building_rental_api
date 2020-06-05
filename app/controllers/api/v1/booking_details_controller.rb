36ce2ea2c54e566b692f8c82db7b2def0f8d0b7f
Ahmed <ahmedgagan6@gmail.com>
2020-06-05 22:34:25 +0530

    changed bundler

--- app/controllers/api/v1/booking_details_controller.rb
+++ app/controllers/api/v1/booking_details_controller.rb
@@ -3,6 +3,8 @@ module Api
     class BookingDetailsController < ApplicationController
 
       def index
+        p "time"
+        p Time.now
         boking_details = BookingDetail.joins("INNER JOIN unit_details b on booking_details.unit_id=b.id INNER JOIN users c on booking_details.booked_by_user_id=c.id").select('booking_details.id as booking_id, booking_details.name as customer_name , c.contact as agent_contact, booking_details.contact as booking_contact, *').where('is_booked=true and booking_details.is_active=true')
         render json: {status: '1', msg: 'All booking details Loaded', data: boking_details}, status: :ok
       end
